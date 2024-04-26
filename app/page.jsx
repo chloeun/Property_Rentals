a61c240b322da711a6fa67b862230afa76daf492
김정은 <chloe@gimjeong-euns-MacBook-Air.local>
2024-04-26 11:02:18 +0900

    add nav bar and hero

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,14 @@
+import Hero from "@/components/Hero"
+import InfoBoxes from "@/components/InfoBoxes"
+import HomeProperties from "@/components/HomeProperties"
+
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <>
+      <Hero />
+      <InfoBoxes />
+      <HomeProperties />
+    </>
   )
 }
 
